b90595a6c6a209748c40403724c0032731a05371
Ben Moore <ben@ben.gs>
2019-05-21 20:14:47 +0100

    Playing with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
 /*jshint esversion: 9 */
 
-let gulp = require('gulp');
-let cleanCSS = require('gulp-clean-css');
+const { series } = require('gulp');
 
-gulp.task('minify-css', () => {
-    // Folder with files to minify
-    return gulp.src('src/css/*.css')
-    //The method pipe() allow you to chain multiple tasks together 
-    //I execute the task to minify the files
-   .pipe(cleanCSS())
-   //I define the destination of the minified files with the method dest
-   .pipe(gulp.dest('dist'));
-});
+function clean() {
+
+    cb();
+}
+
+
+exports.build = series(clean);
