ef62f444a48358f4a93ba6b9315dd7fc703b77b8
Ben Moore <ben@ben.gs>
2019-05-21 19:34:33 +0100

    Update build script

--- build/jenkinsbuild.sh
+++ build/jenkinsbuild.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-touch ~/.profile && source ~/.profile;
-. ~/.nvm/nvm.sh
-. ~/.profile
-. ~/.bashrc
-nvm current || echo "SSH NVM is being installed" &&  touch ~/.profile && curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh -o install_nvm.sh && bash install_nvm.sh && source ~/.profile
-
-echo "checking nvm"
-bash ~/.nvm/nvm.sh;
-nvm --version || exit 1;
-
-echo "Checking Node"
-nvm use default || nvm install node || nvm use default || exit 1
-
 echo "Is Gulp Installed?"
 gulp --version || npm install -g gulp || exit 1;
 
