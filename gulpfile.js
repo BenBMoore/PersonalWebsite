e848c70213a4fa40f302416231c871a830ce80d7
Ben Moore <ben@ben.gs>
2019-05-23 17:18:20 +0100

    Updates to gulpfile/index to allow revision rewriting in extneral files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,60 @@
 /*jshint esversion: 9 */
 
-const { series } = require('gulp');
+const { series, parallel, src, dest } = require('gulp');
+const del = require('delete');
+const cleanCSS = require('gulp-clean-css');
+const cleanJS = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const rev = require('gulp-rev');
+const revRewrite = require('gulp-rev-rewrite');
 
-function clean() {
 
+function clean(cb) {
+    del(['dist/*']);
+    del(['build/*']);
     cb();
 }
 
+function minimiseCSS() {
+    return src('src/css/*.css')
+        .pipe(concat('style.css'))
+        .pipe(cleanCSS())
+        .pipe(rename('style.min.css'))
+        .pipe(dest('build/css/'));
+}
 
-exports.build = series(clean);
+function minimiseJS() {
+    return src('src/js/*.js')
+        .pipe(cleanJS())
+        .pipe(rename({
+            suffix: '-min'
+        }))
+        .pipe(dest('build/js/'));
+}
+
+function revision() {
+        return src('build/**/*.*')
+        .pipe(rev())
+        .pipe(dest('dist/'))
+        .pipe(rev.manifest())
+        .pipe(dest('build/'));
+
+}
+
+function revisionRewrite(){
+    const manifest = src('build/rev-manifest.json');
+    
+    return src('src/index.html')
+    .pipe(revRewrite({manifest}))
+    .pipe(dest('dist/'));
+
+}
+
+function copy() {
+    return src('./src/images/*')
+        .pipe(dest('dist/images/'));
+}
+
+
+exports.build = series(clean, minimiseCSS, minimiseJS, revision, revisionRewrite, copy);
