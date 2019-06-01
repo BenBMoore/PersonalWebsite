2ea591cff934f65da096f2b805612ec8c548a306
Ben Moore <ben@ben.gs>
2019-06-01 23:08:03 +0100

    Update to work with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import './css/uikit.css';
+import './css/style.css';
 import './images/background.jpg';
 
 // loads the Icon plugin
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
   mode:'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'inline-source-map',
+  
   plugins: [
-    new CleanWebpackPlugin()
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template:'./src/index.html',
+      favicon: './src/images/favicon.ico'
+    }),
+    new webpack.HashedModuleIdsPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
         use:[
-          'style-loader',
-          'css-loader'
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+        'css-loader'
         ]
     },
     {
@@ -37,5 +50,17 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist'
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
   }
 };
