ca8dbbd5f7264250ee0862357d2057195be1e99d
benbmoore <blueredorange@gmail.com>
2020-03-26 17:03:47 +0000

    Change to ui-kit imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
-// import '../node_modules/uikit/dist/css/uikit.css';
+import '../node_modules/uikit/dist/css/uikit.min.css';
+import '../node_modules/uikit/dist/css/uikit-core.min.css';
 import './css/style.css';
 import './images/background.jpg';
 
