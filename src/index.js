72575b1f01c41aa1802c92ca0fcc50d79c8ea2cc
benbmoore <blueredorange@gmail.com>
2020-03-26 16:55:29 +0000

    Update to dependencies Link to external UI Kit cdn due to issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
-import './css/uikit.css';
+// import '../node_modules/uikit/dist/css/uikit.css';
 import './css/style.css';
 import './images/background.jpg';
 
